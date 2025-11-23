f39391eaea58108943a0c8931ad265f64ac95f69
Muralikrishna-vaitla <muralikrishnavaitla6883@gmail.com>
2025-11-23 18:00:13 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World! V4";
+    private static final String MESSAGE = "Hello World! V5";
 
     public App() {}
 
